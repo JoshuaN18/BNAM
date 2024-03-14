709ca29bc1d26e8fe8bfc0529afac211d47fd549
Joshua Nam <60495431+JoshuaN18@users.noreply.github.com>
2024-03-13 21:48:11 -0600

    Added update, delete and permissions to User

--- BNAM/User/urls.py
+++ BNAM/User/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import GetUserAPIView
+from .views import GetUserAPIView, UpdateUserAPIView, UserDeleteAPIView
 from . import views
 
 urlpatterns = [
     path("", GetUserAPIView.as_view(), name="list_users"),
     path("<uuid:id>/", GetUserAPIView.as_view(), name="get_user"),
+    path("update/<uuid:id>/", UpdateUserAPIView.as_view(), name="update_user"),
+    path("delete/<uuid:id>/", UserDeleteAPIView.as_view(), name="delete_user")
 ]
