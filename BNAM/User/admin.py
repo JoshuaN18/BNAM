b6a761582f792f72887d1a28863f8660c08b691e
Joshua Nam <60495431+JoshuaN18@users.noreply.github.com>
2024-03-16 16:22:11 -0600

    Added admin panel

--- BNAM/User/admin.py
+++ BNAM/User/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from Budget.models import Budget
 
 UserProfile = get_user_model()
-admin.site.register(UserProfile)
+
+class BudgetInline(admin.TabularInline):
+    model = Budget
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = [
+        BudgetInline,
+    ]
+    search_fields = ['userprofile']
+
+admin.site.register(UserProfile, UserAdmin)
