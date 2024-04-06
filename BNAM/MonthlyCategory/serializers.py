c75971980f9dd5cea48eb42b2c7d4139debcd200
Joshua Nam <jbyn448@gmail.com>
2024-04-06 13:04:53 -0600

    Added get call with month and year and added postgres mac doc

--- BNAM/MonthlyCategory/serializers.py
+++ BNAM/MonthlyCategory/serializers.py
@@ -1,17 +1,32 @@
 from rest_framework import serializers
 from .models import MonthlyCategory
+from Category.models import Category  # Import Category model
 from Category.serializers import CategorySerializer
 
 class MonthlyCategorySerializer(serializers.ModelSerializer):
-    category = CategorySerializer()
+    category_id = serializers.UUIDField(write_only=True)  # Add this field for category UUID input
+
     class Meta:
         model = MonthlyCategory
-        fields = ['monthly_category_id', 'monthly_category_name', 'month_year', 'activity', 'assigned', 'category','user']
-        read_only_fields = ['monthly_category_id', 'user', 'category']
+        fields = ['monthly_category_id', 'monthly_category_name', 'month', 'year', 'activity', 'assigned', 'category_id', 'user']
+        read_only_fields = ['monthly_category_id', 'user']
 
     def to_representation(self, instance):
         rep = super().to_representation(instance)
         rep['category'] = CategorySerializer(instance.category).data
         return rep
 
-    
+    def create(self, validated_data):
+        # Extract and remove category_id from validated_data
+        category_id = validated_data.pop('category_id')
+
+        # Get or create category using category_id
+        category = Category.objects.get(category_id=category_id)
+
+        # Assign category to validated_data
+        validated_data['category'] = category
+
+        # Call super() to create MonthlyCategory instance
+        monthly_category = super().create(validated_data)
+
+        return monthly_category
